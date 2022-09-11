9f84a0502ee52ba186cd33ebff890efa2e59aadf
Michal Lauer <michal.lauer.25@gmail.com>
2022-09-12 01:12:06 +0200

    Excluded public/, added gitmodule, added input removal after build

--- R/build2.R
+++ R/build2.R
@@ -1,2 +1,6 @@
 # An optional custom script to run after Hugo builds your site.
 # You can delete it if you do not need it.
+files <- list.dirs("public/posts")
+out <- grepl(x = files,pattern = "\\/input")
+
+unlink(files[out], recursive = T)
